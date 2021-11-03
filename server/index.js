ba028e54a4278d1a79ca0a4bd7fb14429e20e97b
davman808 <da.seyder@gmail.com>
2021-11-03 09:50:26 +0100

    Added Server and Client apps

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import * as http from "http";
+import express, {request} from "express"
+const app = express()
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.
+})
+app.listen(port, () => {
+    console.log('go here dumbass: http://localhost:8080')
+})
